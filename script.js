58608691e83f92cd2109132693f6b7ebb0042aa4
Shreya-1403 <67495439+Shreya-1403@users.noreply.github.com>
2023-07-19 21:50:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+//let input=prompt("Enter a String");
+let ans=input[0];
+for (let i = 1; i < input-1; i++) {
+	if(input[i]!=input[i+1])
+		ans=input[i];
+}
+return ans;
